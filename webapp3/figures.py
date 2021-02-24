33d1acf946569a3b34016adb7682c725391d4b0d
Andreas Dorta <eichi@gmx.ch>
2021-02-24 12:23:49 +0100

    Add colored graph to webapp3

--- webapp3/figures.py
+++ webapp3/figures.py
@@ -1,6 +1,78 @@
 import plotly.graph_objects as go
 import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
 
+CS=[
+    [0, "rgb(49,54,149)"],
+    [0.1111111111111111, "rgb(69,117,180)"],
+    [0.2222222222222222, "rgb(116,173,209)"],
+    [0.3333333333333333, "rgb(171,217,233)"],
+    [0.4444444444444444, "rgb(224,243,248)"],
+    [0.5555555555555556, "rgb(254,224,144)"],
+    [0.6666666666666666, "rgb(253,174,97)"],
+    [0.7777777777777778, "rgb(244,109,67)"],
+    [0.8888888888888888, "rgb(215,48,39)"],
+    [1.0, "rgb(165,0,38)"]
+]
+
+def new_x_y(x, y, n, kind='linear'):
+    """
+    Returns an interpolation of type kind f with n data points based on y = f(x)
+    
+    x, y: arrays defining y = f(x)
+    n: the number of interpolated date points returned
+    kind: type of interpolation ('linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic',
+    'previous', 'next', where 'zero', 'slinear', 'quadratic' and 'cubic')
+    """
+    f_x = interp1d(x, y)
+    x_new = np.linspace(x[0], x[-1], n)
+    y_new = f_x(x_new)
+    return x_new, y_new
+
+def add_trace(fig, x, y, sz, cs, cmin, cmax, hi=None, sl=True):
+    fig.add_trace(
+        go.Scatter(
+            x=x,
+            y=y,
+            mode='markers',
+            marker={
+                'color': y,
+                'cmin': cmin,
+                'cmax': cmax,
+            'colorscale': cs, 
+            'size': sz,
+        },
+        hoverinfo=hi,
+        showlegend=sl,
+        )
+    )
+
+def get_figure(x, y, n, sz1, sz2, cs, cmin, cmax, convert_ts=False):
+    x_new, y_new = new_x_y(x, y, n)
+    if convert_ts:
+        x = pd.to_datetime(x, unit='s')
+        x_new = pd.to_datetime(x_new, unit='s')
+    fig = go.Figure()
+    add_trace(fig, x_new, y_new, sz1, cs, cmin, cmax, hi='skip', sl=False)
+    add_trace(fig, x, y, sz2, cs, cmin, cmax, sl=False)
+    fig.update_layout(
+        autosize=True,
+        # width=200,
+        # height=100,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0,
+            pad=4
+        ),
+        xaxis_visible=False,
+        yaxis_visible=False,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+    )
+    return fig
 
 def serve_figure():
     x = np.arange(10)
--- webapp3/layouts.py
+++ webapp3/layouts.py
@@ -4,7 +4,19 @@ import dash_bootstrap_components as dbc
 # from dash.dependencies import Input, Output, State
 # from dataloader import DataProviderSingleton
 # import pandas as pd
-from figures import serve_figure
+from figures import serve_figure, get_figure, CS
+from data import load_data
+
+
+
+
+# FIXME do this properly
+df = load_data()['28-032197791b3c']
+df2 = df.iloc[::-60].copy()
+x = df2.timestamp.values
+y = df2.temp_C.values
+fig = get_figure(x, y, 3000, 5, 5, CS, 19, 24, convert_ts=True)
+
 
 card_content_1 = [
     # dbc.CardHeader("Aktuelle Temperatur"),
@@ -61,7 +73,6 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
-
 def serve_layout():
     return html.Div([
         navbar,
@@ -79,9 +90,17 @@ def serve_layout():
                 ),
                 dbc.Row(
                     [
-                        dbc.Col(dbc.Card(card_content_1, color="light", outline=True),
-                                className="col-12 mt-3 mb-3"),
-                    ],
+                        dbc.Col(
+                            dbc.Card(
+                                dbc.CardBody(
+                                    children=[
+                                        dcc.Graph(figure = fig)
+                                    ]
+                                )
+                            ),
+                            className="col-12 mt-3 mb-3",
+                        )
+                    ]
                 )
             ],
             fluid=True
